9f819db717ae4aa89d449e42e2302949bbd4a317
sdimasa <131792268+sdimasa@users.noreply.github.com>
2023-04-25 18:38:38 +0300

    Add files via upload

---
+++ JS2.js
@@ -0,0 +1,8 @@
+alert("Hello world!");       alert("Glory to Ukraine!")      
+document.write("Hello world!")//прописує в документ
+/alert(8+
+10
++12);
+[8,3,5].fortach(alert)
+/*Приклад з тьома повідомленнями.
+Це - багаторядковий коментар. */
